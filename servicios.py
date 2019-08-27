8e7f8bde9e0bd3e188a15a2f17051853a8926b09
stiag0 <stiago01r@gmail.com>
2019-08-27 16:53:32 -0700

    agregado metodo set y delet ademas de optimizar el top30%

--- servicios.py
+++ servicios.py
@@ -11,18 +11,18 @@ tipo_medicion ={'sensor':'DHT11','variable':'humedad','unidades': '%' }
 
 mediciones =[
     {'fecha': '2019-08-10 11:24:08',**tipo_medicion,'valor':0.85},
-    {'fecha': '2019-08-11 13:24:08',**tipo_medicion,'valor':0.75},
     {'fecha': '2019-08-12 12:24:08',**tipo_medicion,'valor':0.6},
     {'fecha': '2019-08-13 10:24:08',**tipo_medicion,'valor':0.73},
     {'fecha': '2019-08-14 13:24:08',**tipo_medicion,'valor':0.71},
+    {'fecha': '2019-08-11 13:24:08',**tipo_medicion,'valor':0.75},
     {'fecha': '2019-08-15 08:24:08',**tipo_medicion,'valor':0.67},
     {'fecha': '2019-08-16 09:24:08',**tipo_medicion,'valor':0.72}
 ]
-@app.route('/')
+@app.route('/',methods= ['GET'])
 def get():
     return jsonify (tipo_medicion)
 
-@app.route('/todo')
+@app.route('/todo',methods= ['GET'])
 def getA():
     return jsonify(mediciones)
         
@@ -34,16 +34,33 @@ def postOne():
 # **body **tipo_medicion
     mediciones.append({**body,**tipo_medicion})
     return jsonify(mediciones)
-@app.route('/top30%')
+@app.route('/top30%',methods= ['GET'])
 def topMed():
     total = len(mediciones)
     mostrar = int(total*0.30) 
     top = sorted(mediciones, key = lambda i: i['valor'], reverse= True)
-    while len(top) > mostrar:
-        top.pop()
+    top = top[0:mostrar]
+
 
     return jsonify(top)
 
+@app.route('/modificar',methods=['PUT'])
+def setMedicion():
+    body =request.json
+    for x in mediciones:
+        if body['fecha'] == x['fecha']:
+            x['valor'] = body['valor']
+    return jsonify(mediciones)
+
+@app.route('/eliminar',methods=['PUT'])
+def deleteMedicion():
+    body =request.json
+    i=0
+    for x in mediciones:
+        if body['fecha'] == x['fecha']:
+            mediciones.pop(i)
+        i=i+1
+    return jsonify(mediciones)
 
 app.run(port=5555,debug=True)
     
